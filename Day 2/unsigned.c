01cc1221a26871b055286834cfd14fc43d79f7a6
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2015-08-12 01:42:20 +0530

    Update unsigned.c

--- Day 2/unsigned.c
+++ Day 2/unsigned.c
@@ -3,7 +3,7 @@ int main()
 {
 	unsigned int n=0xFFFFFFFE;   // maximum value of unsigned int -1 (n+1) is the maximum value
 	printf("\n%u\n",n);	     // printing the number (maximum number unsigned int can hold -1)
-	printf("\n%u\n",++n);
+	printf("\n%u\n",++n);        // printing the maximum number (which unsigned int can hold)
 	printf("\n%u\n",++n);
 	return 0;
 }

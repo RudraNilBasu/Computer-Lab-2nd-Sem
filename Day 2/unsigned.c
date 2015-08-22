5e1b0adf6e96bbe538467348ca8c1a0c78a806c7
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2015-08-22 23:44:58 +0530

    Update unsigned.c

--- Day 2/unsigned.c
+++ Day 2/unsigned.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main()
 {
-	unsigned int n=0xFFFFFFFE;   // maximum value of unsigned int -1 (n+1) is the maximum value
-	printf("\n%u\n",n);	     // printing the number (maximum number unsigned int can hold -1)
-	printf("\n%u\n",++n);        // printing the maximum number (which unsigned int can hold)
-	printf("\n%u\n",++n);	     // overflow occurs here
+	unsigned int n=0xFFFFFFFE;   // maximum value of unsigned int -1 (n+1) is the maximum value.
+	printf("\n%u\n",n);	     // printing the number (maximum number unsigned int can hold -1).
+	printf("\n%u\n",++n);        // printing the maximum number (which unsigned int can hold).
+	printf("\n%u\n",++n);	     // overflow occurs here.
 	return 0;
 }

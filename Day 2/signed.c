0d07bd8450a970346fafcace43a8ec33b184c192
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2015-08-24 01:07:08 +0530

    Update signed.c

--- Day 2/signed.c
+++ Day 2/signed.c
@@ -3,7 +3,7 @@ int main()
 {
 	
 	signed int n=0x7FFFFFFF; // The maximum number which signed int can hold.
-	printf("\n%d\n",n);      // printing the signed integer (maximum value)
+	printf("\n%d\n",n);      // printing the signed integer (maximum value).
 	printf("\n%d\n",++n);    // Printing the overflow
 	return 0;
 }
